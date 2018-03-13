ffe1ba81f8d98bcf7540a0f2ec4e200ffca2a9ad
Ricardo Garrido <ricgarridoc@hotmail.com>
2018-03-13 15:46:16 -0600

    I learned how to use public, private, protected, this, new, construct

--- poo/1-1poo.php
+++ poo/1-1poo.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 class MiClaseDePHP
 {
 	//Propiedades
@@ -18,8 +18,77 @@ class MiClaseDePHP
 $a = new MiClaseDePHP;
 $a->accion();
 
+
+*/
 /* MODIFICANDO UNA PROPIEDAD PÚBLICA DESDE EL OBJETO
 echo "<br>";
 echo $a->nombre="Luis";
 */
+
+
+
+
+class imprimir 
+{
+
+	function __construct($var){//Esto es el pinshi constructor
+		echo "Este es un pinshi constructor, un metodo mágico<br> y esta madre se vuelve a imprimir cada vez que se instancia un objeto(instanciar = crear)<br>";
+		$this->apellidoPaterno = $var;
+	}
+	public $nombre = "Ricardo";
+	protected $apellidoPaterno = "Garrido";
+	private $apellidoMaterno = "Cruz";
+	
+	public function loquesea($palabra,$asiatica)
+	{
+		//echo "Hello world!<br>";
+		//echo "$this->apellidoPaterno" . $palabra . $asiatica;
+		echo "$this->apellidoPaterno";
+	}
+
+}
+
+$a = new imprimir('Carmen, me quede en el min 53');
+$a->loquesea('Primer parametro','Segundo parametro');
+//$b = new imprimir;
+//$a -> loquesea(" es la verga!", "ALV, esto lo van a leer en stackoverflow!.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
